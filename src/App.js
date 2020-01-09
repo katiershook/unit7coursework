f784d320f195e6ad949c84a7d0b8f613e0741df2
katiershook <53106073+katiershook@users.noreply.github.com>
2020-01-09 15:55:45 -0800

    reupload

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const desc = 'I just learned how to create a React node and render it into the DOM.';
+const myTitleID = 'main-title';
+const name = 'Katie';
+
+const header = (
+  <header>
+    <h1 id={myTitleID}>{ name }'s First React Element!</h1> 
+    <p className="main-desc">{ desc }</p>
+  </header>
+);
+
+ReactDOM.render(
+  header,
+  document.getElementById('root')
+);
 
 export default App;
